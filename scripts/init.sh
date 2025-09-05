fccb693a736eb361c3e2d3a682eaab21fa7f6c7a
katsu996 <aspiration.934@gmail.com>
2025-09-05 19:58:21 +0900

    remove: dockutil

--- scripts/init.sh
+++ scripts/init.sh
@@ -28,44 +28,5 @@ defaults write com.apple.menuextra.clock DateFormat -string "M\u6708d\u65e5(EEE)
 echo "バッテリーの残量をパーセント表示"
 defaults write com.apple.menuextra.battery ShowPercent YES
 
-echo "Dockを操作"
-slp="sleep 5"
-dockutil --remove Siri
-$slp
-dockutil --remove リマインダー
-$slp
-dockutil --remove カレンダー
-$slp
-dockutil --remove 連絡先
-$slp
-dockutil --remove iBooks
-$slp
-dockutil --remove FaceTime
-$slp
-dockutil --remove Mail
-$slp
-dockutil --remove メール
-$slp
-dockutil --remove Maps
-$slp
-dockutil --remove Messages
-$slp
-dockutil --remove メッセージ
-$slp
-dockutil --remove Safari
-$slp
-dockutil --remove "Photo Booth"
-$slp
-dockutil --remove 写真
-$slp
-dockutil --remove マップ
-$slp
-dockutil --remove Pages
-$slp
-dockutil --remove Numbers
-$slp
-dockutil --remove Keynote
-$slp
-
 # echo 'Rebooting to reflect settings'
 # sudo shutdown -r now
