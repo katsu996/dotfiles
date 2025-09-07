4751a7ace50987e34cad7e9d2f239b09bcbea617
katsu996 <aspiration.934@gmail.com>
2025-09-07 16:19:38 +0900

    feat: 再起動処理を追加

--- scripts/init.sh
+++ scripts/init.sh
@@ -28,5 +28,22 @@ defaults write com.apple.menuextra.clock DateFormat -string "H:mm:ss"
 echo "バッテリーの残量をパーセント表示"
 defaults write com.apple.menuextra.battery ShowPercent YES
 
-# echo 'Rebooting to reflect settings'
-# sudo shutdown -r now
+echo "設定を反映するには再起動が必要です。再起動しますか？ (Y/N)"
+
+# 入力を受け取る
+read answer
+
+# 入力された内容を小文字に変換
+answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+
+# 入力内容によって分岐
+if [ "$answer" = "y" ]; then
+    echo "再起動します..."
+    sudo shutdown -r now
+elif [ "$answer" = "n" ]; then
+    echo "処理を終了します。"
+    exit 0
+else
+    echo "無効な入力です。処理を終了します。"
+    exit 1
+fi
